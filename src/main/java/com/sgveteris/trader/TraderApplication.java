c3dbc5539c33b89fc78536aa18fe865a3d804972
Doruk Altan <doruk.altan@ditravo.com>
2021-03-14 21:37:13 +0300

    EnableFeignClients annotation added tnto TraderApplication

--- src/main/java/com/sgveteris/trader/TraderApplication.java
+++ src/main/java/com/sgveteris/trader/TraderApplication.java
@@ -2,12 +2,13 @@ package com.sgveteris.trader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class TraderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TraderApplication.class, args);
 	}
-
 }
