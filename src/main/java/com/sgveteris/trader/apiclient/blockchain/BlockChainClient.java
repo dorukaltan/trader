53845ed959c658527d48da477063730a8099980c
Doruk Altan <doruk.altan@ditravo.com>
2021-03-14 23:57:08 +0300

    get api url from property and misplaced cacheable removed

--- src/main/java/com/sgveteris/trader/apiclient/blockchain/BlockChainClient.java
+++ src/main/java/com/sgveteris/trader/apiclient/blockchain/BlockChainClient.java
@@ -1,15 +1,13 @@
 package com.sgveteris.trader.apiclient.blockchain;
 
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "blockChain", url = "https://blockchain.info")
+@FeignClient(name = "blockChain", url = "${blockChain.apiUrl}")
 public interface BlockChainClient {
 
-	@Cacheable("blockChainRates")
 	@RequestMapping(method = RequestMethod.GET, value = "/ticker") 
 	ResponseEntity<BlockChainTickerResponseModel> getExchangeRates();
 
